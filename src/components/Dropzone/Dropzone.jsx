e4fde39a3152805bc1bfe298ac2f6ab644299af4
Alan Churichi <achurichi@gmail.com>
2024-05-31 10:54:45 +0200

    Add dropzone

---
+++ src/components/Dropzone/Dropzone.jsx
@@ -0,0 +1,36 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+import { BsPlusLg } from "react-icons/bs";
+
+import styles from "./Dropzone.module.scss";
+
+const Dropzone = ({
+  onAdd,
+  accept,
+  activeMessage = "Drop the files here...",
+  defaultMessage = "Drag and drop files, or click to select",
+}) => {
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      onAdd(acceptedFiles);
+    },
+    [onAdd],
+  );
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept,
+    onDrop,
+  });
+
+  return (
+    <div className={styles.container} {...getRootProps()}>
+      <input {...getInputProps()} />
+      <BsPlusLg size={30} strokeWidth={0.25} />
+      <div className={styles.text}>
+        {isDragActive ? activeMessage : defaultMessage}
+      </div>
+    </div>
+  );
+};
+
+export default Dropzone;
